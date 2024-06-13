525bdcd55de4997ec701b0b37c56554b46bdda09
TaKi <TaKi@gendalf.ru>
2024-06-13 17:38:42 +0300

    new logic

--- src/ContentRatingAPI/Application/ContentLoader/ContentLoaderService.cs
+++ src/ContentRatingAPI/Application/ContentLoader/ContentLoaderService.cs
@@ -1,33 +1,40 @@
-﻿using SharpCompress.Common;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace ContentRatingAPI.Application.ContentLoader
 {
     public class ContentLoaderService
     {
-        public async Task SaveToHLS(string path, string outPath, string name)
+        public async Task SaveToHLS(byte[] videoBytes, string outPath, string name)
         {
-            ConvertToHls(path, Path.Combine(outPath, "hls", name));
+            ConvertToHls(videoBytes, Path.Combine(outPath, "hls", name));
         }
-        private void ConvertToHls(string inputPath, string outputPath)
+        private void ConvertToHls(byte[] videoData, string outputPath)
         {
+            var arguments = $"-i - -c:v libx264 -b:v 1M -hls_time 30 -hls_list_size 0 -r 20 -preset ultrafast -hls_segment_filename \"{outputPath}%03d.ts\" \"{outputPath}.m3u8\"";
 
-            var arguments = $"-i \"{inputPath}\" -c:v libx264 -b:v 1M -hls_time 10 -hls_list_size 0 -hls_segment_filename \"{outputPath}%03d.ts\" \"{outputPath}.m3u8\"";
-
-            var process = new Process
+            using var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = @"ffmpeg.exe",
+                    FileName = @"ffmpeg",
                     Arguments = arguments,
                     RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false,
-                    CreateNoWindow = true,
+                    CreateNoWindow = false,
+
                 }
             };
-
+            process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (sender, e) => Console.WriteLine("ERROR: " + e.Data);
             process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            using (Stream stdin = process.StandardInput.BaseStream)
+            {
+                stdin.Write(videoData, 0, videoData.Length);
+            }
             process.WaitForExit();
         }
     }

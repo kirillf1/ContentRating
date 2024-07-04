dc81826741330f9b5f92753960d6ade96e32f36d
kirillf1 <kirillf2412@gmail.com>
2024-07-04 22:25:58 +0300

    added handling result

--- src/ContentRatingAPI/Infrastructure/MediatrBehaviors/GlobalRequestExceptionHandler.cs
+++ src/ContentRatingAPI/Infrastructure/MediatrBehaviors/GlobalRequestExceptionHandler.cs
@@ -1,23 +1,23 @@
-﻿using MediatR.Pipeline;
+﻿using Ardalis.Result;
+using MediatR.Pipeline;
+using System.Reflection;
 
 namespace ContentRatingAPI.Infrastructure.MediatrBehaviors
 {
-    public class GlobalRequestExceptionHandler<TRequest, TResponse, TException, T> : IRequestExceptionHandler<TRequest, TResponse, TException>
-       where TResponse : Result<T>
+    public class GlobalRequestExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
        where TException : Exception
     {
-        private readonly ILogger<GlobalRequestExceptionHandler<TRequest, TResponse, TException, T>> _logger;
+        private readonly ILogger<GlobalRequestExceptionHandler<TRequest, TResponse, TException>> _logger;
         public GlobalRequestExceptionHandler(
-           ILogger<GlobalRequestExceptionHandler<TRequest, TResponse, TException, T>> logger)
+           ILogger<GlobalRequestExceptionHandler<TRequest, TResponse, TException>> logger)
         {
             _logger = logger;
         }
         public Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state,
             CancellationToken cancellationToken)
         {
-            
             _logger.LogError(exception, "Something went wrong while handling request of type {@requestType}", typeof(TRequest));
-            state.SetHandled(default);
+            state.SetHandled(CreateErrorResult(typeof(TResponse), exception.Message));
             return Task.CompletedTask;
         }
         private TResponse? GetExceptionResult(string requestName)
@@ -27,6 +27,17 @@ namespace ContentRatingAPI.Infrastructure.MediatrBehaviors
             return (TResponse?)Activator.CreateInstance(typeof(TResponse));
 
             return default;
+        }
+        public static TResponse? CreateErrorResult(Type type, string errorMessage)
+        {
+            //Type resultType = type.MakeGenericType(type);
+            ConstructorInfo ctor = type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { typeof(ResultStatus) }, null);
+            object result = ctor.Invoke(new object[] { ResultStatus.Error });
+
+            PropertyInfo errorsProperty = type.GetProperty("Errors");
+            errorsProperty.SetValue(result, new string[] { errorMessage });
+
+            return (TResponse)result;
         }
     }
 }
